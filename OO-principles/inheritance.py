14dffed08370450d6fa07cb4f28db4b8d7c9ffa3
Breir28 <104933579+Breir28@users.noreply.github.com>
2025-07-13 14:18:33 +0200

    Update inheritance.py

--- OO-principles/inheritance.py
+++ OO-principles/inheritance.py
@@ -1,1 +1,19 @@
+class Tier:
+    def sprich(self):
+        return "Laut"
 
+class Hund(Tier):
+    def sprich(self):
+        return "Wuff"
+    
+class Katze(Tier):
+    def sprich(self):
+        return "Miau"    
+
+# Ausführung
+t = Tier()
+h = Hund()
+k = Katze()
+print("Tier:", t.sprich())  # Tier: Laut
+print("Hund:", h.sprich())  # Hund: Wuff
+print("Katze:", k.sprich())  # Katze: Miau
